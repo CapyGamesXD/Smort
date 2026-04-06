8827a433e8500fd6dd18e9ebf7fcd917166dba56
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-04-06 19:33:13 +0100

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	//@ts-nocheck
+	import { onMount } from 'svelte';
+
+	let objects = $state();
+
+	function loop() {
+		for (let i = 0; i < objects.length; i++) {
+			objects[i].posX += Math.floor(Math.random() * 10 - 5);
+			objects[i].posY += Math.floor(Math.random() * 10 - 5);
+		}
+		requestAnimationFrame(loop);
+	}
+
+	onMount(() => {
+		objects = Array.from({ length: 50 }, () => ({
+			posX: 150,
+			posY: window.innerHeight / 2
+		}));
+		loop();
+	});
+</script>
+
+<div class="centerdiv">
+	<svg>
+		{#each objects as object}
+			<circle r="5" cx={object.posX} cy={object.posY} fill="red" />
+		{/each}
+	</svg>
+</div>
